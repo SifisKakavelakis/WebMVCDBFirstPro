406f9e088227b95f51986f4d0d20a30891865af5
Iosif Kakavelakis <sifiskakavelakis2004@gmail.com>
2026-04-03 19:36:10 +0300

    chore: move folder Filters

--- SchoolApp/Filters/UserFilterDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SchoolApp.Core.Filters
-{
-    public class UserFiltersDTO
-    {
-        public string? Username { get; set; }
-        public string? Email { get; set; }
-        public string? UserRole { get; set; }
-    }
-}
